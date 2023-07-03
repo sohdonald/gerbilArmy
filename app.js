e9cab2475e288ea998172f9427cfc3771635d80d
Donald Soh <sohdonald@gmail.com>
2023-07-03 18:06:19 -0400

    post stop working agin

--- app.js
+++ app.js
@@ -8,30 +8,29 @@
 //const fetchGerbilP = fetch("http://localhost:3000/gerbilPrivates/")
 // fetchGerbilP will have a promise object value
 //console.log(fetchGerbilP);
-  // fetch returns promise, promise is object data type
-  /* Js uses promises to represent completion or failure
+// fetch returns promise, promise is object data type
+/* Js uses promises to represent completion or failure
   of asynchronous operation*/
-  // promises have 3 states, pending, fulfilled, and rejected
-  //pending is initial state, determined later
-  // fulfilled is state that meets what the code requested
-  // rejected does not meet the request
+// promises have 3 states, pending, fulfilled, and rejected
+//pending is initial state, determined later
+// fulfilled is state that meets what the code requested
+// rejected does not meet the request
 
-  /* Asynchronous programming is a way to perform
+/* Asynchronous programming is a way to perform
   a long-running task while other parts of the 
   program are running
   */
 
-  // trying to get data from gerbilPrivate object list
-  
-//fetch request first, 
+// trying to get data from gerbilPrivate object list
+
+//fetch request first,
 const fetchGerbilP = fetch("http://localhost:3000/gerbilPrivates/")
- .then((response) => response.json())
- .then((data) => console.log(data))
+  .then((response) => response.json())
+  .then((data) => console.log(data));
 
 function postGPrivates(privateName, privateAge, privateYears) {
   fetch("http://localhost:3000/gerbilPrivates/", {
     method: "POST",
-    
     headers: {
       "Content-Type": "application/json",
       Accept: "application/json",
@@ -41,90 +40,67 @@ function postGPrivates(privateName, privateAge, privateYears) {
     // converts a JavaScript value to a JSON string
 
     body: JSON.stringify({
-     
       name: privateName,
       age: privateAge,
       yearsServed: privateYears,
     }),
   }) //this ends the fetch
-
     .then(function (response) {
       return response.json();
     }) //first .then ends here
 
     // second .then is important
-    
-    .then(() => {
-      // with toytale lab, we were calling back a function
 
-      // function newPrivate() {
-      //   // what does newPrivate do? Do we even need it?
-      //   const getId = object.id;
-      //   const getPrivateName = object.name;
-      //   const getPrivateAge = object.age;
-      //   const getPrivateYears = object.yearsServed;
-      // }
-     
-      // console.log(newPrivate);
-      // document.body.append(newPrivate);
-
-     
-        
-        console.log(postPrivUserInput);
-        //console log is not working
-        // we need something to get access to the user input
-  
+    .then((newPrivate) => {
+      const gerbilPrivatesContainer = document.querySelector("#gerbilPrivates");
+      const gerbilParagraph = document.createElement("p");
+      gerbilParagraph.textContent = newPrivate.name;
+      gerbilPrivatesContainer.append(gerbilParagraph);
     }) // the second .then ends here
 
+    // with toytale lab, we were calling back a function
+    // function newPrivate() {
+    // what does newPrivate do? Do we even need it?
+    //   const getId = object.id;
+    //   const getPrivateName = object.name;
+    //   const getPrivateAge = object.age;
+    //   const getPrivateYears = object.yearsServed;
+    // }
 
-    
+    // console.log(newPrivate);
+    // document.body.append(newPrivate);
+    //console log is not working
+    // we need something to get access to the user input
+
     .catch(function (error) {
       document.body.append(error);
-    }) //this ends .catch
+    }); //this ends .catch
+} //function postGPrivates ends here
 
-  } //function postGPrivates ends here
+const privateForm = document.getElementById("gerbil-private-form");
+privateForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  // what is e.target doing?
+  const newPrivate = Object.fromEntries(new FormData(e.target));
+  console.log(FormData(e.target));
 
-  postGPrivates();
+  postGPrivates(
+    newPrivate.gerbilPrivateName,
+    newPrivate.gerbilPrivateAge,
+    newPrivate.gerbilPrivateYears
+  );
+}); // addEventListener ends here()
 
-  function postPrivUserInput() {
-    // might be a good idea to consolidate all this into postPrivUserInput function
+// we want to post data from newPrivate
+// name, age, yearsServed render
+// use querySelector to get 3 inputs
+// use querySelector to get privateName input
+// we have Object.fromEntries, make array of arrays into an object
+// is this code in the right place?
 
-  const addPrivName = document.querySelector("#gerbilPrivateName")
-  // data type of addPrivName is html input
-  // we want the private's name from this input element
-  const postPrivName = addPrivName.value;
-  // the user input is not showing in db.json
-
-  const addPrivAge = document.querySelector("#gerbilPrivateAge")
-  // we want the private's age from this input element
-  // something is wrong with addPrivAge.value
-  const postPrivAge = addPrivAge.value;
-
-  const addPrivYears = document.querySelector("#gerbilPrivateYears")
-  // we want the private's yearsServed from this input element
-  // something is wrong with .value
-  const postPrivYears = addPrivYears.value;
-
-  } // end of postPrivUserInput
-
-  document
-  .getElementById("gerbil-private-form")
-  .addEventListener("submit", function (e) {
-    e.preventDefault();
-    // what is e.target doing?
-    console.log(e.target);
-    // name, age, yearsServed render
-    // use querySelector to get 3 inputs
-    // use querySelector to get privateName input
-  }) // addEventListener ends here
-    
-  
-
-  
-  //dry don't repeat yourself
-  // tech art called this implementation
-  // tech act use dry to reduce mouse click
-
+//dry don't repeat yourself
+// tech art called this implementation
+// tech act use dry to reduce mouse click
 
 // [] patch method makes changes or updates to object
 
